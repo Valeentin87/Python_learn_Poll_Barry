f0151d35a4a1c5affb39b52370fc02cf070864d5
Valeentin87 <valeentin87@list.ru>
2022-11-08 00:40:32 +0300

    проверили версию установленного python

--- odd.py
+++ odd.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from os import getcwd
+import sys
 odds = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
 
 right_this_minute = datetime.today().minute
@@ -11,3 +12,5 @@ else:
 
 where_am_i = getcwd()
 print(where_am_i)
+
+print(sys.version)
