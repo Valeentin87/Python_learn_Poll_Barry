46967956edb5da89de5692e1d875adfdb0855460
Valeentin87 <valeentin87@list.ru>
2022-11-16 14:25:44 +0300

    добавили вторую декорированную функцию, возвращающую в браузер результат поиска вхождения букв в строку

--- Chapter5_construction_web_application/hello_flask.py
+++ Chapter5_construction_web_application/hello_flask.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask   # импортируем класс Flask из модуля flask
+from vsearch import search4letters
 
-app = Flask(_name_)
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
+app = Flask(__name__)    # создание экземпляра объекта Flask и присваивание его переменной app
 
-app.run()
+@app.route('/')     # декоратор функции настраивает поведение функции без изменения кода самой функции, функция становится декорированной
+def hello() -> str:         # декоратор route через переменную app позволяет связать веб-путь URL c функцией на Python
+    return 'Hello world from Flask!'  # далее декоратор route возвращает результат выполнения функции ожидающему веб серверу, а тот в свою очередь веб-браузеру
+
+@app.route('/search4')
+def do_search() -> str:
+    '''Функция возвращает через декоратор веб-серверу localhost(127.0.0.1) а тот в свою очередь веб браузеру
+    результат работы функции search4letters из созданного нами ранее модуля vsearch при обращении по URL localhost/search '''
+    result_set = search4letters('life, the universe, and everything!', 'eiru,!')
+    result_str = ''.join(list(result_set))
+    return result_str
+app.run()   # предлагает объекту Flask запустить веб-сервер в переменной app используя метод run
